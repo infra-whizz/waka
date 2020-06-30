18ad791014fc8ebe121b0b4fc2fa03034860cc9b
Bo Maryniuk <bo@maryniuk.net>
2020-07-01 00:20:54 +0200

    Add filesystem formatter API

--- fsmake.go
+++ fsmake.go
@@ -1,5 +1,11 @@
 package waka
 
+import (
+	"fmt"
+
+	wzlib_subprocess "github.com/infra-whizz/wzlib/subprocess"
+)
+
 /*
 	Prepares the image file system.
 	The whole purpose of the fsmake is just create an image,
@@ -8,14 +14,30 @@ package waka
 	Design of this class is subject to change in a future.
 */
 
-type WakaFSMake struct{}
+type WakaFSMake struct {
+	sizeMb      int64
+	rawFilename string
+}
 
 func NewWakaFSMake() *WakaFSMake {
 	fm := new(WakaFSMake)
 	return fm
 }
 
-func (fm *WakaFSMake) Format(fstype string) {
+func (fm *WakaFSMake) SetSizeMb(size int64) *WakaFSMake {
+	fm.sizeMb = size
+	return fm
+}
+
+func (fm *WakaFSMake) SetOutputFile(fname string) *WakaFSMake {
+	fm.rawFilename = fname
+	return fm
+}
+
+func (fm *WakaFSMake) Format(fstype string) error {
+	if fm.sizeMb == 0 {
+		return fmt.Errorf("Unable to format image with %s to a zero size", fstype)
+	}
 	switch fstype {
 	case "ext4":
 		fm.ext4()
@@ -27,7 +49,10 @@ func (fm *WakaFSMake) Format(fstype string) {
 		fm.ext2()
 	case "cramfs":
 		fm.cramfs()
+	default:
+		return fmt.Errorf("Unknown FS type: %s", fstype)
 	}
+	return nil
 }
 
 // Format image with ext2 FS
@@ -42,6 +67,7 @@ func (fm *WakaFSMake) ext3() {
 
 // Format image with ext4 FS
 func (fm *WakaFSMake) ext4() {
+	fmt.Println("Formatting raw image with ext4")
 	fm.createRawImage()
 }
 
