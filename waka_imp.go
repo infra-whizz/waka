d3d5547e19b1e70f1ead868f8824552111bd7cfd
Bo Maryniuk <bo@maryniuk.net>
2020-12-10 13:56:11 +0100

    Dump into error log output from the wzd (todo: better handling!)

--- waka_imp.go
+++ waka_imp.go
@@ -2,15 +2,16 @@ package waka
 
 import (
 	"fmt"
-
-	wzlib_subprocess "github.com/infra-whizz/wzlib/subprocess"
+	"path"
+	"strings"
 
 	waka_diskman "github.com/infra-whizz/waka/diskman"
+	wzlib_subprocess "github.com/infra-whizz/wzlib/subprocess"
 )
 
 // Prepare environment, make disks, mount
 func (w *Waka) prepare(force bool) {
-	w.diskman = waka_diskman.NewWkDiskManager(w.imageLayout).SetTempDir(".")
+	w.diskman = waka_diskman.NewWkDiskManager(w.imageLayout).SetTempDir(".").SetBuildOutput(w.outputPath)
 	w.imageSetup = NewWakaImageSetup(w.diskman, w.imageLayout)
 	if force {
 		ExitOnError(w.diskman.Remove())
@@ -34,10 +35,19 @@ func (w *Waka) format() {
 	ExitOnErrorPreamble(w.diskman.FormatAllPartitions(), "Formatting failure -")
 }
 
+func (w *Waka) listRootFs() {
+	root, _ := w.imageSetup.getRootPartitionMountpoint()
+	cmd, _ := wzlib_subprocess.BufferedExec("find", root)
+	content := cmd.StdoutString()
+	cmd.Wait()
+	fmt.Println("---------\nContent:\n", content, "---------")
+}
+
 // Bootstrap basic components, setup CMS system
 func (w *Waka) preProvision() {
 	ExitOnErrorPreamble(w.imageSetup.CopyPayload(), "Unable to copy payload")
 	ExitOnErrorPreamble(w.imageSetup.CopyPreRootFs(), "Unable to copy pre-install rootfs layout")
+	ExitOnErrorPreamble(w.imageSetup.CopyWhizzLocal(), "Unable to prepare whizz daemon")
 }
 
 func (w *Waka) postProvision() {
@@ -46,10 +56,31 @@ func (w *Waka) postProvision() {
 
 // RunCMS on prepared image mount
 func (w *Waka) runCMS() {
+	rootfs, _ := w.imageSetup.getRootPartitionMountpoint()
+	wzdBinPath := path.Join(rootfs, "tmp", ".waka", "bin", "wzd")
+	collection := path.Join(rootfs, "tmp", ".waka", "collection")
+
+	caller := wzlib_subprocess.NewEnvBufferedExec().SetEnv("WAKA_MOUNT", rootfs)
+	cmd, err := caller.Exec(wzdBinPath, "local", "-d", collection, "-s", "init")
+	ExitOnErrorPreamble(err, "Unable to run Whizz locally")
+	stout := strings.Split(cmd.StdoutString(), "\n")
+	sterr := strings.Split(cmd.StderrString(), "\n")
+	cmd.Wait()
+
+	for _, line := range stout {
+		line = strings.TrimSpace(line)
+		w.GetLogger().Info(line)
+	}
+
+	for _, line := range sterr {
+		line = strings.TrimSpace(line)
+		w.GetLogger().Error(line)
+	}
 }
 
 // Cleanup image, devices, data etc
 func (w *Waka) cleanup() {
+	// w.listRootFs() // debug
 	ExitOnErrorPreamble(w.diskman.Umount(), "Unable to umount devices")
 	ExitOnErrorPreamble(w.diskman.LoopOff(),
 		fmt.Sprintf("Umount loop device %s failed -", w.diskman.GetDiskImageDevice()))
