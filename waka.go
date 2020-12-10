7fc5addf62b636d3bd2cb914e35c96e8e9016abb
Bo Maryniuk <bo@maryniuk.net>
2020-12-10 13:55:21 +0100

    Add build outpuyt and cleanup functions

--- waka.go
+++ waka.go
@@ -3,16 +3,35 @@ package waka
 import (
 	waka_diskman "github.com/infra-whizz/waka/diskman"
 	waka_layout "github.com/infra-whizz/waka/layout"
+	wzlib_logger "github.com/infra-whizz/wzlib/logger"
 )
 
 type Waka struct {
 	imageLayout *waka_layout.WkImageLayout
 	diskman     *waka_diskman.WkDiskManager
 	imageSetup  *WakaImageSetup
+
+	outputPath    string
+	cleanupMounts bool
+
+	wzlib_logger.WzLogger
 }
 
 func NewWaka() *Waka {
 	w := new(Waka)
+	w.cleanupMounts = true
+	return w
+}
+
+// SetCleanupOnExit turns off or on cleaning up (if not crashed prematurely)
+func (w *Waka) SetCleanupOnExit(cleanup bool) *Waka {
+	w.cleanupMounts = cleanup
+	return w
+}
+
+// SetBuildOutput other than $LAYOUT/build
+func (w *Waka) SetBuildOutput(outputPath string) *Waka {
+	w.outputPath = outputPath
 	return w
 }
 
@@ -31,5 +50,10 @@ func (w *Waka) Build(force bool) {
 	w.preProvision()
 	w.runCMS()
 	w.postProvision()
-	w.cleanup()
+
+	if w.cleanupMounts {
+		w.cleanup()
+	} else {
+		w.GetLogger().Info("Leaving mounts untouched for debugging purposes")
+	}
 }
